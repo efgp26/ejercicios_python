9d42f91dc3e7bdc5dc0bd1d173b8c2ab0b6fe972
efgp26 <garciapatinoedwar26@gmail.com>
2021-07-23 22:20:15 -0500

    add the file verificacion numero where you create the algorithm select the positive, negative and nule number

---
+++ verificacion_numero.py
@@ -0,0 +1,10 @@
+numero = float(input("ingrese un numero: "))
+
+if numero < 0:
+    print("el numero es negativo")
+
+elif numero == 0:
+    print("el numero es nulo")
+
+else:
+    print("el numero es positivo")
